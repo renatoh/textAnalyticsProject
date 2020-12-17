2cbe16ea4b8c1e594dda9b01a5225cc4024c8a6b
Renato Haeberli <renato.haeberli@cec.valantic.com>
2020-12-17 23:42:40 +0100

    enhance file collecting script, add articles for year 2020

--- src/extract_news_article.py
+++ src/extract_news_article.py
@@ -1,14 +1,20 @@
 import json
+import os
+
 import requests
+import uuid
 
 # url = 'https://content.guardianapis.com/search'
 # api_key = '?pages=30&api-key=389d51f6-216c-4143-a91e-500a36e3303d'
-
-
+year = '2020'
 api_key = 'api-key=389d51f6-216c-4143-a91e-500a36e3303d'
 url1 = 'https://content.guardianapis.com/search?page-size=50&page=1&%s' % api_key
 
 
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 def do_http_call(url_to_call):
@@ -16,27 +22,47 @@ def do_http_call(url_to_call):
     response.raise_for_status()
     return response.json()
 
-def write_json_to_file(file_name, json_content):
-    with open('../resources/articles/2020-03-01/'+file_name, 'w') as f:
+def write_json_to_file(json_content, start_date):
+    dic = '../resources/articles/' + start_date + '/'
+    ensure_dir(dic)
+    with open(dic + str(uuid.uuid4()) + '.json', 'w') as f:
         json.dump(json_content, f)
 
-url_with_filter = ('https://content.guardianapis.com/search?page-size=50&page=%s&' + api_key + '&to-date=2020-03-30&from-date=2020-03-01')
-url_details = 'https://content.guardianapis.com/politics/live/2020/dec/15/uk-coronavirus-live-covid-christmas-rules-restrictions-brexit-latest-updates?show-fields=bodyText,headline&%s' % api_key
 
-initial_result = do_http_call(url_with_filter % str(1))
+def add_leadding_zero(i):
+    return "0" + str(i) if i < 10 else i
 
-nr_pages = int(initial_result['response']['pages'])
 
-for i in range(1, nr_pages + 1):
-    print("nr of pages:" + str(i)+ " of "+ str(nr_pages))
-    search_result = do_http_call(url_with_filter % str(i))
+for i in range(1, 13):
 
-    for result in search_result['response']['results']:
+    month = add_leadding_zero(i)
 
-        api_url = result['apiUrl']
-        print('calling:' + api_url)
-        article = do_http_call(api_url + '?' + api_key +'&show-fields=bodyText,headline')
-        id = article['response']['content']['id']
-        id = id.replace('/','-') # we do not want to have '/' in the file name
-        write_json_to_file(id + '.json', article)
+    days_of_month = [1,5,9,11,13,16,19,20,24,28]
+
+    folder_name =  start_date = year + '-' + str(month) + '-01'
+
+    for day in days_of_month:
+
+        day = add_leadding_zero(day)
+
+        start_date = year + '-' + str(month) + '-' + str(day)
+        end_date = year + '-' + str(month )+ '-' + str(day)
+        url_with_filter = ('https://content.guardianapis.com/search?page-size=20&page=%s&' + api_key + '&from-date='+start_date+'&to-date=' + end_date)
+        url_details = 'https://content.guardianapis.com/politics/live/2020/dec/15/uk-coronavirus-live-covid-christmas-rules-restrictions-brexit-latest-updates?show-fields=bodyText,headline&%s' % api_key
+
+        initial_result = do_http_call(url_with_filter % str(1))
+
+        nr_pages = int(initial_result['response']['pages'])
+
+        print("nr of page:" + str(i) )
+        search_result = do_http_call(url_with_filter % str(1))
+
+        for result in search_result['response']['results']:
+
+            api_url = result['apiUrl']
+            print('calling:' + api_url)
+            article = do_http_call(api_url + '?' + api_key +'&show-fields=bodyText,headline')
+            id = article['response']['content']['id']
+            id = id.replace('/','-') # we do not want to have '/' in the file name
+            write_json_to_file(id + '.json', article, folder_name)
 
---
+++ src/testIt.py
@@ -0,0 +1,8 @@
+
+import uuid
+
+print(uuid.uuid4())
+
+
+for i in range(1,13):
+    print(i)
